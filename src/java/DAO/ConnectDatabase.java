ab51c31aa5d6b71c15de80dfe792a9be080599c7
Your Name <khaikaka2k2@gmail.com>
2024-03-26 18:21:37 +0700

    fix them thanh vien

--- src/java/DAO/ConnectDatabase.java
+++ src/java/DAO/ConnectDatabase.java
@@ -11,7 +11,7 @@ public class ConnectDatabase {
         String dbURL = "jdbc:mysql://localhost:3306/manage_library?allowPublicKeyRetrieval=true&useSSL=false";
 //		String dbURL = "jdbc:mysql://node238128-lemanhltm.j.layershift.co.uk/manage_library?characterEncoding=UTF-8";
         String username = "root";
-        String password = "Tunham123456789";
+        String password = "12345";
 //		String password = "KROmim21616";
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = (Connection) DriverManager.getConnection(dbURL, username, password);
